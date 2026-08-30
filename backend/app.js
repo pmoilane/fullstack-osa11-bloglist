7f22d0a537020a1ac84ca408c980cc08d04f4736
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-08-30 21:54:31 +0300

    periodic health check and version bump

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 app.get('/version', (req, res) => {
-  res.send('1')
+  res.send('2')
 })
 
 app.get('/health', (req, res) => {
