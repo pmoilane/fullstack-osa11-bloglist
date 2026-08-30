2bca98ae7af3aa3d8c3f62c0076abcba6fd503f5
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-08-30 20:55:49 +0300

    add deployment to pipeline, and version and health endpoints to monitor deployment

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,14 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
